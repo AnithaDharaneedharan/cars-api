71cd2231e5fa6df1049035a480b91548356023c6
AnithaD <anitha.k.dharaneedharan@gmail.com>
2020-12-04 00:37:54 +0000

    Gets all cars and by id

--- routes/cars.js
+++ routes/cars.js
@@ -2,15 +2,27 @@ const express = require('express');
 const router = express.Router();
 const Car = require('../models/Car');
 
+// Gets all cars
 router.get('/', async (req, res) => {
   try {
       const cars = await Car.find();
-      res.json(cars)
+      res.send(200).json(cars);
   } catch (err) {
       res.json({message: err})
   }
 });
 
+// Gets a  specific car 
+router.get('/:carId', async (req, res) => {
+    try {
+        const car = await Car.findById(req.params.carId);
+        res.send(200).json(car);
+    } catch (err) {
+        res.json({message: err})
+    }
+  });
+
+// Submits a car
 router.post('/', async (req, res) => {
   const car = new Car({
     make: req.body.make,
