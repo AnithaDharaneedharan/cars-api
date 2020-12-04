f37222455368ded9dcacad91fa7c657334383ee8
AnithaD <anitha.k.dharaneedharan@gmail.com>
2020-12-04 00:26:28 +0000

    Adds GET and POST operations

--- routes/cars.js
+++ routes/cars.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const Car = require('../models/Car');
 
-router.get('/', (req, res)=>{
-    res.send(' are on cars')
-})
+router.get('/', async (req, res) => {
+  try {
+      const cars = await Car.find();
+      res.json(cars)
+  } catch (err) {
+      res.json({message: err})
+  }
+});
+
+router.post('/', async (req, res) => {
+  const car = new Car({
+    make: req.body.make,
+    model: req.body.model,
+    colour: req.body.colour,
+  });
+  try {
+    const savedPost = await car.save();
+    res.send(200).json(savedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
 
 module.exports = router;
